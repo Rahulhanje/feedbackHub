250ed321a62a0d4dd35d5016cfa9894d93252856
Rahul <rahulhanje0.7@gmail.com>
2024-10-08 16:17:31 +0530

    Hear signin button error removed mob  -v

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -10,31 +10,32 @@ import { Button } from "./ui/button";
 import Headermenu from "./headermenu";
 const Pageheader = () => {
   return (
-    <header className="sticky inset-x-0 top-0 z-50 border backdrop-blur-lg bg-white/60 border-white/30">
-    <div className="w-full max-w-screen-xl px-2.5 lg:px-20 relative mx-auto">
-      <div className="flex h-14 items-center justify-between px-2">
-        <Image src="/LOGO.png" alt="logo" width="380" height="250" />
-  
-        <div>
-          <SignedOut>
-            <SignInButton>
-              <Button className="">Sign In</Button>
-            </SignInButton>
-            <SignUpButton>
-              <Button className="ml-2">Sign Up</Button>
-            </SignUpButton>
-          </SignedOut>
-          <SignedIn>
-            <div className="flex">
-              <Headermenu />
-              <UserButton />
-            </div>
-          </SignedIn>
+    <header className=" w-full sticky inset-x-0 top-0 z-50 border backdrop-blur-lg bg-white/60 border-white/30">
+      <div className="w-full max-w-screen-xl px-2.5 lg:px-20 relative mx-auto">
+        <div className="flex h-14 items-center justify-between px-2">
+          <Image src="/LOGO.png" alt="logo" width="300" height="250" />
+
+          <div>
+            <SignedOut>
+              <div className="flex">
+                <SignInButton>
+                  <Button className="">Sign In</Button>
+                </SignInButton>
+                <SignUpButton>
+                  <Button className="ml-2">Sign Up</Button>
+                </SignUpButton>
+              </div>
+            </SignedOut>
+            <SignedIn>
+              <div className="flex">
+                <Headermenu />
+                <UserButton />
+              </div>
+            </SignedIn>
+          </div>
         </div>
       </div>
-    </div>
-  </header>
-  
+    </header>
   );
 };
 
